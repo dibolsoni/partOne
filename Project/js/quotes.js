04504171a0583dc060752a5f616c7816a84d6b0d
decebe <dibolsoni@gmail.com>
2019-04-21 12:49:18 -0300

    finished project part one for Techdegree from Team Tree House

---
+++ Project/js/quotes.js
@@ -0,0 +1,48 @@
+
+//quotes got at https://www.goalcast.com/2017/03/29/top-30-most-inspiring-albert-einstein-quotes/
+
+var quotes = [
+    {
+        quote: "Few are those who see with their own eyes and feel with their own hearts.",
+        source: "Albert Einstein",
+        citation: "",
+        year: null,
+        tags: ['Motivational'] 
+    },
+    {
+        quote: "Imagination is more important than knowledge. Knowledge is limited. Imagination encircles the world.",
+        source: "Albert Einstein",
+        citation: "",
+        year: null,
+        tags: ['Inpiration', 'Study']
+    }
+];
+
+// quote got at https://www.keepinspiring.me/bob-marley-quotes/
+quotes.push(
+        {
+            quote: "Don’t gain the world and lose your soul. Wisdom is better than silver and gold.",
+            source: "Bob Marley",
+            citation: "MUSIC - Zion Train, from the album Uprising ",
+            year: 1980,
+            tags: ['Inpiration', 'Motivational', 'music'] 
+        },
+    );
+    
+    //quote got at https://www.brainyquote.com/authors/elon_musk
+quotes.push(
+        {
+            quote: "If you get up in the morning and think the future is going to be better, it is a bright day. Otherwise, it's not.",
+            source: "Elon Musk",
+            citation: "",
+            year: null,
+            tags: ['Motivational', 'Business'] 
+        },
+        {
+            quote: "When something is important enough, you do it even if the odds are not in your favor",
+            source: "Elon Musk",
+            citation: "",
+            year: null,
+            tags: ['Motivational', 'Inpiration', 'Business'] 
+        },
+);
--- Project/js/script.js
+++ Project/js/script.js
@@ -6,51 +6,67 @@ project 1 - A Random Quote Generator
 // Study guide for this project - https://drive.google.com/file/d/1s5grutGuQFwJcQP8bFwEI69Q8FCkGdDk/view?usp=sharing
 
 
+//quote number index / -1 when loads the page
+var quoteNum = -1;
+
+//returns a different quote as the published one
+function getRandomQuote() {
+  randomQuoteNum = Math.floor(Math.random() * Math.floor(quotes.length));
+  while (quoteNum === randomQuoteNum) {
+    randomQuoteNum = Math.floor(Math.random() * Math.floor(quotes.length))
+  }
+  quoteNum = randomQuoteNum;
+  return quotes[quoteNum]
+};
+
 /*** 
-  Create the array of quote objects and name it `quotes`.
-  Add at least five quote objects to the `quotes` array.
-  Give each quote object a `quote` and `source` property.
-  Add the `citation` property to at least one object in the array.
-  Add the `year` property to at least one object in the array.
-  Use console.log() to log your array of quotes to the console.
+ prints the quote in quote-box div 
 ***/
+function printQuote() {
+  quoteObj = getRandomQuote();
+  quoteDiv = document.getElementById('quote-box');
+  var html = '<p class="quote">' + quoteObj.quote + '</p>';
+  html += '<p class="source">' + quoteObj.source + '</p>';
+  if (quoteObj.citation) {
+    html += '<span class="citation">' + quoteObj.citation + '</span>';
+  };
+  if (quoteObj.year) {
+    html += '<span class="year">' + quoteObj.year + '</span>';
+  };
+  if (quoteObj.tags) {
+    quoteObj.tags.forEach(item => {
+      html += '<br /><span class="tags">' + item + '</span>';
+    });
+  }
+  html += '</div>';
 
+  quoteDiv.style.backgroundColor = randomColor(0.2);
+  quoteDiv.innerHTML = html;
 
-
+};
 
 /***
-  Create the `getRandomQuote` function to:
-   - Create a variable to store a random number 
-   - Cse the random number to `return` a random quote object from the `quotes` array.
-***/
+ * random green color of the background changing
+ */
+function randomRGB() {
+  colorNumber = Math.floor(Math.random() * 256 + 1)
+  return colorNumber;
+}
 
+function randomColor(alpha=1) {
+  return 'rgba(' + 
+          randomRGB() + ',' + 
+          randomRGB() + ',' + 
+          randomRGB() + ',' +
+          alpha +
+          ')'
+}
 
+//call the function
+printQuote();
+window.setInterval(printQuote, 20000);
 
-
-/***
-  Create the `printQuote` function to: 
-   - Call the `getRandomQuote` function and assign it to a variable.
-   - Create a variable for the HTML string and set it equal to an empty string.
-   - Use the HTML template in the instructions or the markup in the index.html file, AND 
-     the random quote vairable to build your HTML string.
-   - Add the quote and source section to the HTML string.
-   - Use an if statement to check for the citation property before adding it to the HTML string.
-   - Use an if statement to check for the year property before adding it to the HTML string.
-   - Don't forget to close that final `p` tag.
-   - Set the `innerHTML` of the `quote-box` div to the HTML string. 
-***/
-
-
-
-
-/***
-  When the "Show another quote" button is clicked, the event listener 
-  below will be triggered, and it will call, or "invoke", the `printQuote` 
-  function. So do not make any changes to the line of code below this 
-  comment.
-***/
-
+//add a event listener click on the button
 document.getElementById('loadQuote').addEventListener("click", printQuote, false);
 
 
-// Remember to delete the comments that came with this file, and replace them with your own code comments.
